24acf61ff8623cb6f8c04134a2a2f50bfe66cff7
Allan Julie <allanzinhofontes@gmail.com>
2021-03-07 12:36:40 -0300

    models created

--- API/src/controllers/UsersController.ts
+++ API/src/controllers/UsersController.ts
@@ -1,11 +1,41 @@
 import { Request, Response } from "express";
-
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersController {
   async create(request: Request, response: Response) {
-    const { body } = request;
-    console.log(body);
-    response.json({message: "user created!"})
+    const { 
+      user_name,
+      first_name,
+      last_name,
+      account_code,
+      password
+    } = request.body;
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userNameAlreadyExists = await usersRepository.findOne({
+      user_name
+    });
+
+    if(userNameAlreadyExists) {
+      response
+      .status(400)
+      .json({
+        error: "user already exists"
+      });
+    }
+    
+    const userCreated = usersRepository.create({
+      user_name,
+      first_name,
+      last_name,
+      account_code,
+      password
+    });
+
+    await usersRepository.save(userCreated);
+
+    response.status(201).json(userCreated);
   }
 }
 
---
+++ API/src/models/User.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_name: string;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  account_code: number;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { User };
